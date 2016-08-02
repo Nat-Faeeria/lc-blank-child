23adce2329f0b34f8751ced00acaa6805ddd6f1a
Nat-Faeeria <nathanael.gimenez@serli.com>
2016-08-02 10:24:47 +0900

    modified things in the display of custom content types

--- single-intern.php
+++ single-intern.php
@@ -6,7 +6,7 @@
             <div class="top-margin"></div>
             <div class="post-header" style="background: linear-gradient(rgba(88,88,88, 0.4),rgba(88,88,88, 0.4)), url('<?php echo wp_get_attachment_image_src(get_post_thumbnail_id())[0]; ?>') center scroll no-repeat; background-size: cover;">
                 <h1><?php the_title(); ?></h1>
-                <span class="bold"><?php _e("Posted on ")?><time datetime="<?php echo esc_attr(get_the_date('c')); ?>"><?php echo esc_html(get_the_date()); ?></time> <?php _e(" by "); the_author() ?></span>
+                <span class="bold"><?php _e("Posted on ")?><time datetime="<?php echo esc_attr(get_the_date('c')); ?>"><?php echo esc_html(get_the_date()); ?></time></span>
             </div>
             <div class="post-content">
                 <?php the_content(); ?>
